3a75648634834ac5b25daae9fbfda8cd7a0e9d67
Nabil71-dev <69987319+Nabil71-dev@users.noreply.github.com>
2021-05-27 14:18:03 +0600

    Update func.js

--- src/component/func.js
+++ src/component/func.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./components.css";
-const Post = (props) => {
-  if (props.data.length === 0) {
+const Post = (data) => {
+  if (data.length === 0) {
     return <h1>No post published yet</h1>;
   } else {
     return (
       <div className="post_container">
-        {props.data.map((d) => (
+        {data.map((d) => (
           <div className="posts" key={d.id}>
             <h1>{d.title}</h1>
             <p className="posts_date">{d.readable_publish_date}</p>
